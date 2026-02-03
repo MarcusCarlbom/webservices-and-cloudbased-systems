4b13927e781b88f1711feb0f5873f3918c6f8ad5
MarcusCarlbom <marcus@carlbom.cc>
2026-02-03 12:08:05 +0100

    Refactor URL shortening service: improve ID generation and error handling

--- assignment1/assingment1.py
+++ assignment1/assingment1.py
@@ -3,6 +3,26 @@ from flask import Flask, request, jsonify, redirect
 app = Flask(__name__)
 
 url_database = {}
+id_counter = 0
+
+# Based on the link https://mojoauth.com/binary-encoding-decoding/base62-with-python#encoding-data-to-base62
+def base62_encode(num):
+    if num == 0:
+        return '0'
+    base62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = []
+    while num > 0:
+        remainder = num % 62  
+        result.append(base62[remainder])
+        num = num // 62
+    return ''.join(reversed(result))
+
+
+def generate_id():
+    global id_counter
+    id_counter += 1
+    return base62_encode(id_counter)
+
 
 @app.route('/', methods=['POST'])
 def shorten_url():
@@ -13,6 +33,7 @@ def shorten_url():
     new_id = generate_id()
     url_database[new_id] = url
     return jsonify({'id': new_id}), 201
+
 
 # Get only stored URL ids and not the correlated links, per specification in assignment. 
 @app.route('/', methods=['GET'])
@@ -30,17 +51,17 @@ def redirect_url(id):
 def update_url(id):
     data = request.get_json()
     new_url = data.get('url')
-    if data.get('url') is None:
-        return jsonify({'error': 'No URL provided'}), 400
     if id not in url_database:
-        return jsonify({'error': 'ID not found'}), 404
+        return '', 404
+    if new_url is None:
+        return jsonify({'error': 'No URL provided'}), 400
     url_database[id] = new_url
     return '', 200
 
 @app.route('/<id>', methods=['DELETE'])
 def delete_url(id):
     if id not in url_database:
-        return jsonify({'error': 'ID not found'}), 404
+        return '', 404
     del url_database[id]
     return '', 204
 
